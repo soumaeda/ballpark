d6f234ec0a578ca3dcb102570d08dcc367d4086c
Ubuntu <ubuntu@ip-172-31-18-193.ap-northeast-1.compute.internal>
2023-04-11 14:48:26 +0000

    Add Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,18 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[session][:password])
-      
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
-      
+      flash.now[:danger] = "メールアドレスかパスワードが間違っています"
       render 'new', status: :unprocessable_entity
     end
   end
   
   def destroy
-    
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
