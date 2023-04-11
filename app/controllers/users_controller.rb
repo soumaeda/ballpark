8db5c4f0d177c6420e010b2edb037877ca9457df
Ubuntu <ubuntu@ip-172-31-18-193.ap-northeast-1.compute.internal>
2023-04-11 07:29:19 +0000

    Finish User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = "新規登録完了"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
