194925e9aab265114fc654f8d6530d81a4597664
Ubuntu <ubuntu@ip-172-31-18-193.ap-northeast-1.compute.internal>
2023-04-12 15:05:33 +0000

    Add gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  has_one_attached :profile_image
   
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/person.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.valiant(resize_to_limit: [100, 100]).processed
-  end
+  
+  
 end
