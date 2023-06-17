edc510dad55534f3f04819f08070c5283e70c5ef
Ubuntu <ubuntu@ip-172-31-18-193.ap-northeast-1.compute.internal>
2023-06-17 07:24:06 +0000

    add all image

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,16 @@ class PlacesController < ApplicationController
     @places = [
       { id: 1, name: "東京ドーム", image: 'parks/tokyo_dome.jpg'},
       { id: 2, name: "エスコンフィールド", image: 'parks/esconfield.jpg'},
-      { id: 3, name: "楽天モバイルパーク", image: 'parks/rakuten_mobile_park.jpg'}
+      { id: 3, name: "楽天モバイルパーク", image: 'parks/rakuten_mobile_park.jpg'},
+      { id: 4, name: "メットライフドーム", image: 'parks/mettolife_dome.jpg'},
+      { id: 5, name: "zozoマリンスタジアム", image: 'parks/zozomarine.jpg'},
+      { id: 6, name: "明治神宮球場", image: 'parks/jingu.jpg'},
+      { id: 7, name: "横浜スタジアム", image: 'parks/yokohama.jpg'},
+      { id: 8, name: "バンテリンドーム", image: 'parks/banterin.jpg'},
+      { id: 9, name: "京セラドーム", image: 'parks/kyosera.jpg'},
+      { id: 10, name: "阪神甲子園球場", image: 'parks/kohshien.jpg'},
+      { id: 11, name: "マツダスタジアム", image: 'parks/mazda.jpg'},
+      { id: 12, name: "paypayドーム", image: 'parks/paypay_dome.jpg'}
     ]
   end
   
